a034ea6606ea8d0314935c12fe5df01c07656a7c
Beyza Ünsal <beyza.unsal@boun.edu.tr>
2021-01-23 16:38:09 +0300

    Functions added

--- Source.cpp
+++ Source.cpp
@@ -73,8 +73,9 @@ void PrintTree(Node* pRoot, int Level)
 
 	PrintTree(pRoot->pRight, Level + 1);
 
-	for (int i = 0; i < Level; i++)
+	for (int i = 0; i < Level; i++) {
 		cout << "  ";
+	}
 	cout << pRoot->i << endl;
 
 	PrintTree(pRoot->pLeft, Level + 1);
@@ -91,6 +92,40 @@ void Insert(Node*& pRoot, Node* pNewNode)
 		else
 			Insert(pRoot->pRight, pNewNode);
 	}
+}
+int largestbranch(Node* pRoot)
+{
+	if (!pRoot) {
+		return 0;
+	}
+	int right_sum = largestbranch(pRoot->pRight);
+	int left_sum = largestbranch(pRoot->pLeft);
+	int max_sum = max(right_sum, left_sum);
+	return pRoot->i + max_sum;
+}
+void print_branch(Node* n) {
+	int left = largestbranch(n->pLeft);
+	int right = largestbranch(n->pRight);
+	if (left > right && n->pLeft) {
+		cout << n->pLeft->i<<" ";
+		n = n->pLeft;
+		print_branch(n);
+	}
+	else if (left < right && n->pRight) {
+		cout << n->pRight->i<<" ";
+		n = n->pRight;
+		print_branch(n);
+	}
+
+}
+
+
+
+void Find_max_branch(Node* pRoot) {
+	cout << "Branch with the largest sum is: ";
+	cout << pRoot->i << " ";
+	print_branch(pRoot);
+	cout << "  -> SUM : " << largestbranch(pRoot);
 }
 
 void main()
@@ -109,5 +144,7 @@ void main()
 	}
 
 	PrintTree(pRoot, 1);
+	cout << endl;
+	Find_max_branch(pRoot);
 }
 
